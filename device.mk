f8af0885741aa7b4c935f32a3b1ef3fe5588482f
nullbytepl <nullbytepl@gmail.com>
2022-09-21 22:19:48 +0530

    surya: include fw images
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# XiaomiParts
+$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
+
+# Inherit surya firmware images
+$(call inherit-product, firmware/xiaomi/surya/Android.mk)
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
@@ -415,6 +421,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    firmware/xiaomi/surya \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/xiaomi

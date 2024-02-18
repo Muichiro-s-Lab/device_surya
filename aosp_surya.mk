4f894aafe6b16db7d6cbf8f3037f9443043fa6e5
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2024-02-18 12:16:43 +0800

    surya : HorizonDroid!!!

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -12,9 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Horizondroid Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP  := false
+WITH_GMS := true
+TARGET_ENABLE_BLUR := false
+TARGET_BUILD_VIMUSIC := true
+
+# Horizondroid
+HORIZON_BUILD_TYPE= UNOFFICIAL
+HORIZON_MAINTAINER := Muichiro09
+
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG

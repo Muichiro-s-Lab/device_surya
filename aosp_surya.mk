b47ebf516b4fb47202274fe51968550223278b73
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2024-02-18 12:16:43 +0800

    surya : HorizonDroid!!!

---
+++ aosp_surya.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2020-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from surya device
+$(call inherit-product, device/xiaomi/surya/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/false/config/common_full_phone.mk)
+
+# Horizondroid Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP  := false
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_ENABLE_BLUR := false
+TARGET_INCLUDE_WIFI_EXT := true
+PRODUCT_CHARACTERISTICS := nosdcard
+TARGET_DISABLE_EPPE := true
+TARGET_DEBLOAT := true
+TARGET_BUILD_VIMUSIC := true
+
+# Horizondroid
+HORIZON_BUILD_TYPE= UNOFFICIAL
+HORIZON_MAINTAINER := Muichiro09
+
+PRODUCT_NAME := aosp_surya
+PRODUCT_DEVICE := surya
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := M2007J20CG
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
